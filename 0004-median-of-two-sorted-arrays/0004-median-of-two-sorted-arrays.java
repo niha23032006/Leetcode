81bada8d744a553965d12f9798f4d5e1d1f0b206
Niha M <nniha8549@gmail.com>
2026-07-18 09:19:00 +0530

    Time: 1 ms (100%), Space: 48.7 MB (87.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,42 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
         int m = nums1.length;
         int n = nums2.length;
 
-        int[] arr = new int[m + n];
+        int low = 0, high = m;
 
-        int i = 0, j = 0, k = 0;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
 
-        // Merge the two sorted arrays
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                arr[k++] = nums1[i++];
-            } else {
-                arr[k++] = nums2[j++];
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // Correct partition found
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) +
+                            Math.min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            }
+            else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            }
+            else {
+                low = partitionX + 1;
             }
         }
 
-        while (i < m) {
-            arr[k++] = nums1[i++];
-        }
-
-        while (j < n) {
-            arr[k++] = nums2[j++];
-        }
-
-        int size = m + n;
-
-        if (size % 2 == 0) {
-            return (arr[size / 2] + arr[(size / 2) - 1]) / 2.0;
-        } else {
-            return arr[size / 2];
-        }
+        throw new IllegalArgumentException();
     }
 }
