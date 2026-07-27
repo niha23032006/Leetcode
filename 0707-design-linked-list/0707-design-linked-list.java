a34d156c4354ad4cb9e4865a22b80045c773ccdd
Niha M <nniha8549@gmail.com>
2026-07-27 13:53:56 +0530

    Time: 10 ms (46.33%), Space: 47.2 MB (18.02%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,112 @@
+class MyLinkedList {
+
+    // Node class
+    class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Node head;
+    int size;
+
+    // Initialize the linked list
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    // Get value at index
+    public int get(int index) {
+
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+
+        Node current = head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.val;
+    }
+
+    // Add node at the beginning
+    public void addAtHead(int val) {
+
+        Node newNode = new Node(val);
+
+        newNode.next = head;
+        head = newNode;
+
+        size++;
+    }
+
+    // Add node at the end
+    public void addAtTail(int val) {
+
+        Node newNode = new Node(val);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+
+            Node current = head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+
+        if (index < 0 || index > size) {
+            return;
+        }
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+
+        size++;
+    }
+    public void deleteAtIndex(int index) {
+
+        if (index < 0 || index >= size) {
+            return;
+        }
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+        current.next = current.next.next;
+
+        size--;
+    }
+}
